0682960d120f70e7f0e319617c699a742f8d1842
Tenki <srodriguezdev.l@gmail.com>
2023-05-15 12:03:54 -0500

    Se ajusto el uso del timer, funcionalidades pausa y resume

--- lib/provider/countdown_provider.dart
+++ lib/provider/countdown_provider.dart
@@ -1,18 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class CountDownProvider extends ChangeNotifier {
-  Duration duration = const Duration(seconds: 70);
+  Duration duration = const Duration(seconds: 75);
   // Iniciamos una variable booleana por defecto en falso
   bool isRunnig = false;
+  // iniciamos una variable de tipo StreamSubscription la cual nos va a permitir, suscribir o desuscribir al streamProvider
+  StreamSubscription<int>? _tickSubscription;
   // iniciamos una funcion que nos retorne, si esta o no esta corriendo y por notifique si hay o no un cambio
   void StartStopTimer() {
     // start Timer
     if (!isRunnig) {
+      _startTimer(duration.inSeconds);
     } else {
-      // stop timer or unsuscribe
+      _tickSubscription?.pause();
     }
 
     isRunnig = !isRunnig;
     notifyListeners();
+  }
+
+  void _startTimer(int seconds) {
+    _tickSubscription?.cancel();
+    _tickSubscription = Stream<int>.periodic(
+            const Duration(seconds: 1), (sec) => seconds - sec - 1)
+        .take(seconds)
+        .listen((timeLeftInSecods) {
+      duration = Duration(seconds: timeLeftInSecods);
+      notifyListeners();
+    });
+  }
+
+  String get timeLeftString {
+    // Estamos redondeando los segundos en minutos.
+    final minutes =
+        ((duration.inSeconds / 60) % 60).floor().toString().padLeft(2, '0');
+    final seconds =
+        (duration.inSeconds % 60).floor().toString().padLeft(2, '0');
+    return '$minutes:$seconds';
   }
 }
